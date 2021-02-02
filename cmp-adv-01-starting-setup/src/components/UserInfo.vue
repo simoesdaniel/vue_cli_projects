4f3c1daf8c2175ffefad50c13d06b8f76f8d88c9
Luis Simões <luis.simoes@cetelem.pt>
2021-02-02 18:11:46 +0000

    slot implementation

--- cmp-adv-01-starting-setup/src/components/UserInfo.vue
+++ cmp-adv-01-starting-setup/src/components/UserInfo.vue
@@ -1,35 +1,32 @@
 <template>
   <section>
-    <div>
-      <h3>{{ fullName }}</h3>
-      <base-badge :type="role" :caption="role.toUpperCase()"></base-badge>
-    </div>
-    <p>{{ infoText }}</p>
+    <base-card>
+      <template v-slot:header>
+        <h3>{{ fullName }}</h3>
+        <base-badge :type="role" :caption="role.toUpperCase()"></base-badge>
+      </template>
+      <template v-slot:default>
+        <p>{{ infoText }}</p>
+      </template>
+    </base-card>
   </section>
 </template>
 
 <script>
 export default {
-  props: ['fullName', 'infoText', 'role'],
+  props: ["fullName", "infoText", "role"],
 };
 </script>
 
 // style scoped determines that this styling is fixed to this component and
 cannot affect or be affected by other component stylings
-<style lang="css" scoped>
-section {
-  margin: 2rem auto;
-  max-width: 30rem;
-  border-radius: 12px;
-  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
-  padding: 1rem;
-}
-
-section div {
+<style scoped>
+section header {
   display: flex;
   justify-content: space-between;
   align-items: center;
 }
+
 .badge {
   display: inline-block;
   padding: 0.5rem 1rem;
@@ -37,6 +34,4 @@ section div {
   background-color: #ccc;
   color: #2e2e2e;
 }
-
-
 </style>
