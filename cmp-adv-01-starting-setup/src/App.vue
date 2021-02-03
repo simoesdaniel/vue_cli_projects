6dcc8980732c25a0e079db6920dc97e63e1f1b92
Luis Simões <luis.simoes@cetelem.pt>
2021-02-03 18:14:22 +0000

    scoped slots

--- cmp-adv-01-starting-setup/src/App.vue
+++ cmp-adv-01-starting-setup/src/App.vue
@@ -7,25 +7,34 @@
       :info-text="activeUser.description"
       :role="activeUser.role"
     ></user-info>
+    <!-- scoped slot usage and shorthand for default slot template usage -->
+    <course-goals #default="slotProps">
+        <h2>
+          {{ slotProps.item }}
+        </h2>
+        <p>{{ slotProps.anotherProp }}</p>
+    </course-goals>
   </div>
 </template>
 
 <script>
-import TheHeader from './components/TheHeader';
-import BadgeList from './components/BadgeList';
-import UserInfo from './components/UserInfo';
+import TheHeader from "./components/TheHeader";
+import BadgeList from "./components/BadgeList";
+import UserInfo from "./components/UserInfo";
+import CourseGoals from "./components/CourseGoals";
 export default {
-  components:{
+  components: {
     TheHeader: TheHeader,
     BadgeList: BadgeList,
-    UserInfo: UserInfo
+    UserInfo: UserInfo,
+    CourseGoals: CourseGoals,
   },
   data() {
     return {
       activeUser: {
-        name: 'Maximilian Schwarzmüller',
-        description: 'Site owner and admin',
-        role: 'admin',
+        name: "Maximilian Schwarzmüller",
+        description: "Site owner and admin",
+        role: "admin",
       },
     };
   },
---
+++ cmp-adv-01-starting-setup/src/components/CourseGoals.vue
@@ -0,0 +1,21 @@
+<template>
+  <ul>
+      <li v-for="goal in goals" :key="goal">
+          <!-- this is a scoped slot -->
+          <slot :item="goal" anotherProp="..."></slot>
+      </li>
+  </ul>
+</template>
+
+<script>
+/** Customizable slots [SCOPED SLOTS] */
+export default {
+  data() {
+    return {
+      goals: ["finish the course", "learn vue"],
+    };
+  },
+};
+</script>
+
+<style></style>
