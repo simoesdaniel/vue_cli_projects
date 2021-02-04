ab4bc0c87eff1d88eb7e2a8ff89a13cd69289ae3
Luis Simões <luis.simoes@cetelem.pt>
2021-02-04 12:01:42 +0000

    teleport parts of the DOM

--- cmp-adv-01-starting-setup/src/components/ManageGoals.vue
+++ cmp-adv-01-starting-setup/src/components/ManageGoals.vue
@@ -1,6 +1,44 @@
 <template>
   <div>
     <h2>Manage Goals</h2>
-    <input type="text" />
+    <input type="text" ref="goal" />
+    <!-- teleport allows to move a part of the dom to another location on it 
+    using simple css selectors -->
+    <teleport to="body">
+      <error-alert v-if="inputIsInvalid">
+        <template #default>
+          <h2>Input is Invalid</h2>
+          <p>You should insert a goal name</p>
+          <button @click="dismissError">Dismiss</button>
+        </template>
+      </error-alert>
+    </teleport>
+    <button @click="setGoal">Set this goal</button>
   </div>
 </template>
+
+<script>
+import ErrorAlert from "./ErroAlert";
+export default {
+  components: {
+    ErrorAlert: ErrorAlert,
+  },
+  data() {
+    return {
+      inputIsInvalid: false,
+    };
+  },
+  methods: {
+    setGoal() {
+      const enteredValue = this.$refs.goal.value;
+      if (enteredValue === "") {
+        this.inputIsInvalid = true;
+      }
+    },
+
+    dismissError() {
+      this.inputIsInvalid = false;
+    },
+  },
+};
+</script>
