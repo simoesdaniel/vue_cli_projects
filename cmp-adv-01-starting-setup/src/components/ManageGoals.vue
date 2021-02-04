05299d53208001c10b7ff486fbfde9d48f5142d9
Luis Simões <luis.simoes@cetelem.pt>
2021-02-04 12:31:00 +0000

    vue fragments

--- cmp-adv-01-starting-setup/src/components/ManageGoals.vue
+++ cmp-adv-01-starting-setup/src/components/ManageGoals.vue
@@ -1,20 +1,20 @@
 <template>
-  <div>
-    <h2>Manage Goals</h2>
-    <input type="text" ref="goal" />
-    <!-- teleport allows to move a part of the dom to another location on it 
+  <!-- from vue 2 to vue 3 it was removed the concept of main element wrapper for each vue  
+the representation below has been name fragments. A bunch of html root elements for the component-->
+  <h2>Manage Goals</h2>
+  <input type="text" ref="goal" />
+  <button @click="setGoal">Set this goal</button>
+  <!-- teleport allows to move a part of the dom to another location on it 
     using simple css selectors -->
-    <teleport to="body">
-      <error-alert v-if="inputIsInvalid">
-        <template #default>
-          <h2>Input is Invalid</h2>
-          <p>You should insert a goal name</p>
-          <button @click="dismissError">Dismiss</button>
-        </template>
-      </error-alert>
-    </teleport>
-    <button @click="setGoal">Set this goal</button>
-  </div>
+  <teleport to="body">
+    <error-alert v-if="inputIsInvalid">
+      <template #default>
+        <h2>Input is Invalid</h2>
+        <p>You should insert a goal name</p>
+        <button @click="dismissError">Dismiss</button>
+      </template>
+    </error-alert>
+  </teleport>
 </template>
 
 <script>
